2e3f61b297d8a572c7dff1bf3581e63609acf42d
yunree <contrib@yunree.net>
2020-08-30 23:39:46 +0200

    refactor: Remove unnecessary fish code

--- config.fish
+++ config.fish
@@ -1,8 +1,8 @@
 set ENHANCD_FILTER "sk"
 set GOPATH "$HOME/go"
 set VIRTUAL_ENV_DISABLE_PROMPT 1
+set __fish_git_prompt_color_dirtystate "red"
 
-export STARSHIP_SHELL="fish"
 export VISUAL="nvim"
 export EDITOR="nvim"
 
@@ -11,20 +11,3 @@ alias ls="exa --git"
 
 export GPG_TTY=(tty)
 
-set __fish_git_prompt_color_dirtystate "red"
-# function mkc:
-# 	mkdir $1
-# 	cd $1
-# end
-
-
-# function fish_prompt --description 'Write out the prompt'
-#     switch "$fish_key_bindings"
-#         case fish_hybrid_key_bindings fish_vi_key_bindings
-#             set keymap "$fish_bind_mode"
-#         case '*'
-#             set keymap insert
-#     end
-#     set -l exit_code $status
-#     "/Users/tee-hee/.cargo/bin/starship" prompt --status=$exit_code --keymap=$keymap --cmd-duration=$starship_duration --jobs=(count (jobs -p))
-# end
